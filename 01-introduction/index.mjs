52b87158e8ba0f4a6ed72062e05f6330cd458d43
akm <akm2000@gmail.com>
2022-08-04 18:48:11 +0900

    :shirt: コードを整形

--- 01-introduction/index.mjs
+++ 01-introduction/index.mjs
@@ -1,12 +1,12 @@
 import { chromium } from "playwright-core";
 
 const main = async () => {
-  const browser = await chromium.launch({channel: "chrome", headless: false});
+  const browser = await chromium.launch({ channel: "chrome", headless: false });
   const page = await browser.newPage();
   await page.goto("https://playwright.dev/");
   await page.pause();
   console.log("exit");
   await page.close();
-}
+};
 
 await main();
