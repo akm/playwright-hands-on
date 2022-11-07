72d5c6f81e1f607c2dd7f2620f2a1630cebbb27d
akm <akm2000@gmail.com>
2022-11-07 20:38:47 +0900

    :sparkles: 検索する操作を追加

--- 01-introduction/index.mjs
+++ 01-introduction/index.mjs
@@ -1,10 +1,29 @@
 import { chromium } from "playwright-core";
 
 const main = async () => {
-  const browser = await chromium.launch({ channel: "chrome", headless: false });
+  const browser = await chromium.launch({
+    channel: "chrome",
+    headless: false,
+    slowMo: 1000,
+  });
   const page = await browser.newPage();
   await page.goto("https://playwright.dev/");
   await page.pause();
+  await page.locator("text=Get started").click();
+  await page.pause();
+
+  // Click text=Search⌘K
+  await page.locator("text=Search⌘K").click();
+
+  // Fill [placeholder="Search docs"]
+  await page.locator('[placeholder="Search docs"]').fill("page");
+
+  // Press Enter
+  await page.locator('[placeholder="Search docs"]').press("Enter");
+  await page.waitForURL("https://playwright.dev/docs/pages");
+
+  await page.pause();
+
   await browser.close();
 };
 
